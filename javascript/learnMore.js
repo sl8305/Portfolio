e1e4c12aeb9a42009c8de3a6b1e94bbbaf1b47a7
seattle-mini <sl8305@hotmail.com>
2020-04-06 21:59:23 -0700

    shortened code in LearnMore. More clean look with less lines

--- javascript/learnMore.js
+++ javascript/learnMore.js
@@ -4,36 +4,26 @@
 $(document).ready(function() {
     $("#learnMoreDiv").empty();
 
-    const contact = $('<div>').addClass('marginAuto').addClass("container").attr("id", "bioContact").appendTo("#learnMoreDiv");
-
-    const contactTitle = $('<h2>').text("Learn More");
-    const email = $('<div>').addClass("contactMe");
-    // .text() was not used becuase it does not allow special characteristics such as bolding
-    const emailDescription = $('<p>"If you have any edit suggetions or interesting resources you would like to share with me. Feel free to send me an email. I can be contacted via email: <strong>shad8305@outlook.com</strong> with the subject line github comment."</p>');
-
-
-    contactTitle.appendTo(contact);
-    email.appendTo(contact);
-    emailDescription.appendTo(email);
+    const contact = $('<div id="bioContact" class="marginAuto container" />').appendTo("#learnMoreDiv");
+        const contactTitle = $('<h2>').text("Learn More").appendTo(contact);
+            const email = $('<div class="contactMe" />').appendTo(contact);
+            // .text() was not used becuase it does not allow special characteristics such as bolding
+            const emailDescription = $('<p>"If you have any edit suggetions or interesting resources you would like to share with me. Feel free to send me an email. I can be contacted via email: <strong>shad8305@outlook.com</strong> with the subject line github comment."</p>').appendTo(email);
 
 
-    const linkedIn = $('<div>').addClass("contactMe");
-    const linkedInIconLink = $('<a>').attr("href", "https://www.linkedin.com/in/shad-lee-724621162/");
-    const linkedInImage = $('<img>').attr("src", "images/linkedIn.png").attr("id", "linkedInIcon").addClass("footerIcon");
-    const linkedInDescription = $('<p>').text("The icon here, at the bottom of the page or at the top on the navigation bar will direct you to my LinkedIn page. It will provide more information about me if you are interested in reaching out.");
+        const linkedIn = $('<div class="contactMe" />').appendTo(contact);
+            const linkedInIconLink = $('<a>').attr("href", "https://www.linkedin.com/in/shad-lee-724621162/").appendTo(linkedIn);
+            const linkedInImage = $('<img id="linkedInIcon" class="footerIcon" />').attr("src", "images/linkedIn.png").appendTo(linkedInIconLink);
+                const linkedInDescription = $('<p>')
+                    .text("The icon here, at the bottom of the page or at the top on the navigation bar will direct you to my LinkedIn page. It will provide more information about me if you are interested in reaching out.")
+                    .appendTo(linkedIn);
 
-    linkedInImage.appendTo(linkedInIconLink);
-    linkedInIconLink.appendTo(linkedIn);
-    linkedInDescription.appendTo(linkedIn);
-    linkedIn.appendTo(contact);
 
-    const gitHub = $('<div>').addClass("contactMe");
-    const gitHubLink = $('<a>').attr("href", "https://github.com/sl8305");
-    const gitHubImage = $('<img>').attr("src", "images/githubIcon.svg").attr("id", "gitHubIcon").addClass("footerIcon");
-    const gitHubDescription = $('<p>').text("The icon here, at the bottom of the page or at the top on the navigation bar will direct you to my gitHub page. Here you can see a full list of porjects I have completed or am currently working on.");
+        const gitHub = $('<div class="contactMe" />').appendTo(contact);
+            const gitHubLink = $('<a href="https://github.com/sl8305" />').appendTo(gitHub);
+            const gitHubImage = $('<img id="gitHubIcon" class="footerIcon" />').attr("src", "images/githubIcon.svg").appendTo(gitHubLink);
+            const gitHubDescription = $('<p>')
+                .text("The icon here, at the bottom of the page or at the top on the navigation bar will direct you to my gitHub page. Here you can see a full list of porjects I have completed or am currently working on.")
+                .appendTo(gitHub);
 
-    gitHubImage.appendTo(gitHubLink);
-    gitHubLink.appendTo(gitHub);
-    gitHubDescription.appendTo(gitHub);
-    gitHub.appendTo(contact);
 });
--- javascript/projects.js
+++ javascript/projects.js
@@ -2,7 +2,84 @@
 // Goal is to be able to automate the project cards using the .json file
 
 $(document).ready(function () {
+    $('#indexProjectDiv').empty();
+
+    
 
 
+    const testFile = {
+        projects: [
+            {
+                "src" : "images/crystal.PNG",
+                "alt" : "alternative name",
+                "title" : "testCaseTitle"
+            }
+        ]
+    };
+
+    console.log("Start");
+    console.log(testFile);
+    console.log(testFile.projects[0].title);
+
+    const cardContainer = $('<div class="col-lg-4 col-md-10 col-sm-10"/>');
+    const cardImage = $('<img src='+ testFile.projects[0].src +' alt="testImageAltName">');
+
+
+    cardContainer.appendTo('#indexProjectDiv');
+    cardImage.appendTo(cardContainer);
+    $(cardContainer).append("<p>" + testFile.projects[0].title + "</p>");
+
+
+    // <div class=' col-lg-4 col-md-10 col-sm-10'>
+    //     <div class="card">
+    //         <img class="card-img-top" src="./images/groupProj.PNG" alt="Card image cap">
+    //         <div class="card-body">
+    //             <h5 class="card-title">mResilience</h5>
+    //             <p class="card-text">Single page informational site for another app. The objective of this project was to create a mobile responsive site that followed the requirements provided by the client. This group project was the first experience working with a wireframe and was a great opportunity to polishing up our skills with html, css, web layout design and merging code. </p>
+    //             <a href="https://sl8305.github.io/mResilience/" class="btn btn-primary">Live page</a>
+    //             <a href="https://github.com/sl8305/mResilience" class="btn btn-primary">Github Repo</a>
+    //         </div>
+    //     </div>
+    // </div>
+
+    // <div class=' col-lg-4 col-md-10 col-sm-10'>
+    //     <div class="card">
+    //         <img class="card-img-top" src="./images/homeimage-sm.jpg" alt="Card image cap" 
+    //         style= "width: 40%; align-self: center; " >
+            
+    //         <div class="card-body">
+    //             <h5 class="card-title">Unravel</h5>
+    //             <p class="card-text">Unravel Mobile® is a Cognitive Behavioral Therapy Tool for Patients and Therapists.  Unravel Mobile® is a React Native app that offers an alternative to the traditional method of recording de-stressing events. Unravel Mobile® offers the user the opportunity to log how they are feeling at the touch of a couple of buttons.</p>
+    //             <a href="https://exp.host/@sl8305/UnravelApp" class="btn btn-primary">Live page</a>
+    //             <a href="https://github.com/Unravel-Mobile/Unravel" class="btn btn-primary">Github Repo</a>
+    //         </div>
+    //     </div>
+    // </div>
+
+    // <div class=' col-lg-4 col-md-10 col-sm-10'>
+    //     <div class="card">
+    //         <img class="card-img-top" src="./images/train.png" alt="Card image cap">
+    //         <div class="card-body">
+    //             <h5 class="card-title">Train Scheduler</h5>
+    //             <p class="card-text">First time using moments.js and using a page layout with rows and columns. Train schedules can be input and the list updates with relative times of other trains and the current time. </p>
+    //             <a href="https://sl8305.github.io/train-scheduler/" class="btn btn-primary">Live page</a>
+    //             <a href="https://github.com/sl8305/train-scheduler" class="btn btn-primary">Github Repo</a>
+    //         </div>
+    //     </div>
+    // </div>
+
+
+    // EXTRA
+    // <div class=' col-lg-4 col-md-10 col-sm-10'>
+    //     <div class="card">
+    //         <img class="card-img-top" src="./images/crystal.PNG" alt="Card image cap">
+    //         <div class="card-body">
+    //             <h5 class="card-title">Crystal Numbers Game</h5>
+    //             <p class="card-text">This game exemplifies the usage of random number generators, clickable images and simple game logic. Each crystal has a hidden random value and the player must guess to add up the values to a randomly generated target value. Wins and loses are recorded and a reset mechanism is used to allow for continual fun.  </p>
+    //             <a href="https://sl8305.github.io/gem-game/" class="btn btn-primary">Live page</a>
+    //             <a href="https://github.com/sl8305/gem-game" class="btn btn-primary">Github Repo</a>
+    //         </div>
+    //     </div>
+    // </div> 
 
 });
